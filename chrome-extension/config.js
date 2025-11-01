9e80ad0014482140eeb564a086f955239a2f15dc
ProfLambda <fernando.sanchez@imc.randstad.fr>
2025-11-01 23:23:32 +0100

    modified:   .gitignore 	new file:   chrome-extension/config.js 	modified:   chrome-extension/options.html 	modified:   chrome-extension/options.js

---
+++ chrome-extension/config.js
@@ -0,0 +1,3 @@
+const EXPECTED_HEADER = "ASTATE-EXT-2025-01"; // attendu par l'app php
+const DomainName = "https://astate.pat";
+const APPURL = DomainName + "/astate/index.php";
--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,8 +1,3 @@
-const EXPECTED_HEADER = "ASTATE-EXT-2025-01"; // attendu par l'app php
-
-const DomainName = "https://astate.pat";
-const APPURL = DomainName + "/astate/index.php";
-
 // ----- gestion des onglets -----
 function openTab(tabName) {
 	// Get all elements with class="tabcontent" and hide them
